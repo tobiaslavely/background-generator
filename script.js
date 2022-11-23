381ec969eeb548a685a43f31604c4a957384f21a
Tobias Lavely <tobiaslavely@yahoo.com>
2022-11-23 00:22:55 -0500

    adding starting project

---
+++ script.js
@@ -0,0 +1,40 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+user = {
+	username: "Tobias",
+	passwrod: "Shepoopy",
+}
+
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+database = [
+
+	user = {
+	username: "Tobias",
+	password: "Shepoopy",
+}
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+var newsfeed = [
+
+	user1 = {
+		username: "Spence",
+		timeline: "1"
+	},
+
+	user2 = {
+		username: "Tosh",
+		timeline: "2"
+	},
+
+	user3 = {
+		username: "Timmy Two Step",
+		timeline: "3"
+	},
+
+document.write(database);
